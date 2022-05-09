c6315459a8248d22f278f93b5925292ef750e363
Steve Konves <stephen.konves@gmail.com>
2022-05-09 13:36:31 -0700

    feat: minimum viable code ownership

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,133 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { EOL } from 'os';
+import { join, relative, sep } from 'path';
+
+import * as minimatch from 'minimatch'; // Only needed for githubValiator
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "ownership-vscode" is now active!',
-  );
+let statusBarItem: vscode.StatusBarItem;
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
+function updateStatus(active: vscode.TextEditor | undefined) {
+  if (active) {
+    statusBarItem.text = '$(watch) Owner: running...';
+    statusBarItem.show();
+    process.nextTick(() => {
+      try {
+        githubValidator(active.document.uri.fsPath).then((owner) => {
+          if (
+            active.document.uri.fsPath ===
+            vscode.window.activeTextEditor?.document.uri.fsPath
+          ) {
+            if (owner) {
+              statusBarItem.text = `$(account) Owner: ${owner.name}`;
+            } else {
+              statusBarItem.text = `$(warning) Owner: none`;
+            }
+            statusBarItem.show();
+          }
+        });
+      } catch {
+        if (
+          active.document.uri.fsPath ===
+          vscode.window.activeTextEditor?.document.uri.fsPath
+        ) {
+          statusBarItem.text = `$(error) Owner: error checking ownership!`;
+          statusBarItem.show();
+        }
+      }
+    });
+  } else {
+    statusBarItem.hide();
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
-    'ownership-vscode.helloWorld',
+    'ownership-vscode.validate',
     () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
+      // TODO: do something for real
       vscode.window.showInformationMessage(
         'Hello World from ownership-vscode!',
       );
     },
   );
 
+  statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+  );
+
+  vscode.window.onDidChangeActiveTextEditor(updateStatus);
+  updateStatus(vscode.window.activeTextEditor);
+
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-  // TODO
-}
+type Owner = {
+  filepath: string;
+  name: string;
+  config: string;
+};
+
+type Validator = (filepath: string) => Promise<Owner | undefined>;
+
+/**
+ * Returns randomly mocked ownership data (useful for testing?)
+ * @param filepath path of the file to check
+ * @returns an {@link Owner Owner} object or `undefined` if no owner can be determined
+ */
+const mockValidator: Validator = (filepath) => {
+  const ms = Math.random() * 1000;
+  const noOwner = Math.random() > 0.5;
+  const isError = Math.random() > 0.99;
+
+  return new Promise((res, rej) =>
+    setTimeout(() => {
+      if (isError) rej();
+
+      if (noOwner) res(undefined);
+
+      res({
+        filepath,
+        name: `Some Team`,
+        config: filepath,
+      });
+    }, ms),
+  );
+};
+
+/**
+ * Checks for file ownership by looking at the existing `.github/CODEOWNERS` file
+ * @param filepath path of the file to check
+ * @returns an {@link Owner Owner} object or `undefined` if no owner can be determined
+ */
+const githubValidator: Validator = async (filepath) => {
+  const config = vscode.Uri.parse(
+    join(
+      vscode.workspace.workspaceFolders?.[0]?.uri.fsPath || '',
+      '.github',
+      'CODEOWNERS',
+    ),
+  );
+
+  const rel = `${sep}${relative(
+    vscode.workspace.workspaceFolders?.[0]?.uri.fsPath || '',
+    filepath,
+  )}`;
+
+  const doc = await vscode.workspace.openTextDocument(config);
+
+  const codeowners = doc.getText().split(EOL);
+
+  for (const line of codeowners) {
+    const [pattern, owner] = line.split(' ');
+
+    if (minimatch(rel, pattern)) {
+      return {
+        filepath,
+        name: owner,
+        config: config.fsPath,
+      };
+    }
+  }
+
+  return undefined;
+};
