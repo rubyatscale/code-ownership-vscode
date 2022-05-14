d8fcde2e36b4d97ca715d14963a756beb9a07143
Steve Konves <stephen.konves@gmail.com>
2022-05-13 20:24:46 -0700

    feat: support workspaces with multiple folders

--- src/extension.ts
+++ src/extension.ts
@@ -5,79 +5,56 @@ import { readFile } from 'fs/promises';
 import * as yaml from 'js-yaml';
 import * as vscode from 'vscode';
 
-let ranWithError = false;
-let statusBarItem: vscode.StatusBarItem;
 let channel: vscode.OutputChannel;
-let owner: Owner | undefined = undefined;
 
-function rootWorkspace(): vscode.WorkspaceFolder | undefined {
-  const count = vscode.workspace.workspaceFolders?.length || 0;
-  if (count === 1) {
-    return vscode.workspace.workspaceFolders?.[0];
-  } else {
-    log('warning', `Detected ${count} workspaces, but currently requires 1.`);
-    return undefined;
+function run(file: string | vscode.Uri | null | undefined) {
+  if (!file) {
+    run(vscode.window.activeTextEditor?.document.uri);
+    return;
+  }
+
+  const uri = typeof file === 'string' ? vscode.Uri.parse(file) : file;
+
+  for (const worker of workers.values()) {
+    if (worker.workspaceHas(uri)) {
+      worker.run(uri);
+      break;
+    }
   }
 }
 
-function updateStatus(active: vscode.TextEditor | undefined) {
-  if (ranWithError) {
-    statusBarItem.text = '$(error) Owner: Error!';
-    statusBarItem.tooltip = `See "${channel.name}" output channel for details`;
-    statusBarItem.show();
-  } else if (active) {
-    statusBarItem.text = '$(loading~spin) Owner: running...';
-    statusBarItem.tooltip = undefined;
-    statusBarItem.show();
-    setTimeout(() => {
-      try {
-        codeownershipValidator(active.document.uri.fsPath).then((result) => {
-          owner = result;
-          log('debug', `owner: ${JSON.stringify(owner)}`);
-          if (
-            active.document.uri.fsPath ===
-            vscode.window.activeTextEditor?.document.uri.fsPath
-          ) {
-            if (result) {
-              statusBarItem.text = `$(account) Owner: ${result.teamName}`;
-            } else {
-              statusBarItem.text = `$(warning) Owner: none`;
-            }
-            statusBarItem.show();
-          }
-        });
-      } catch (ex) {
-        log('error', ex.message || ex.toString());
-
-        if (
-          active.document.uri.fsPath ===
-          vscode.window.activeTextEditor?.document.uri.fsPath
-        ) {
-          statusBarItem.text = `$(error) Owner: error checking ownership!`;
-          statusBarItem.show();
-        }
-      }
-    }, 50);
-  } else {
-    statusBarItem.hide();
-  }
-}
+const workers = new Map<string, Worker>();
 
 export function activate(context: vscode.ExtensionContext) {
   channel = vscode.window.createOutputChannel('Code Ownership');
   context.subscriptions.push(channel);
 
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+  );
+  statusBarItem.command = 'code-ownership-vscode.showOwnershipInfo';
+  context.subscriptions.push(statusBarItem);
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('code-ownership-vscode.run', run),
+  );
+
+  const statusProvider = new StatusProvider(statusBarItem);
+  context.subscriptions.push(statusProvider);
+
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'code-ownership-vscode.showOwnershipInfo',
       () => {
-        if (owner) {
-          const filename = owner.filepath.split(sep).slice(-1)[0];
+        if (statusProvider.owner) {
+          const filename = statusProvider.owner.filepath
+            .split(sep)
+            .slice(-1)[0];
 
           vscode.window
             .showInformationMessage(
-              `${filename} is owned by ${owner.teamName}`,
-              ...owner.actions.map((action) => ({
+              `${filename} is owned by ${statusProvider.owner.teamName}`,
+              ...statusProvider.owner.actions.map((action) => ({
                 title: action.title,
                 action() {
                   vscode.commands.executeCommand('vscode.open', action.uri);
@@ -90,25 +67,41 @@ export function activate(context: vscode.ExtensionContext) {
     ),
   );
 
-  const disposable = vscode.commands.registerCommand(
-    'code-ownership-vscode.validate',
-    () => {
-      // TODO: do something for real
-      vscode.window.showInformationMessage(
-        'Hello World from code-ownership-vscode!',
-      );
-    },
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor((active) => {
+      if (active)
+        vscode.commands.executeCommand(
+          'code-ownership-vscode.run',
+          active.document.uri,
+        );
+    }),
   );
 
-  statusBarItem = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Left,
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeWorkspaceFolders(({ added, removed }) => {
+      for (const add of added) {
+        if (workers.has(add.name)) {
+          workers.get(add.name)!.dispose();
+          workers.delete(add.name);
+        }
+
+        workers.set(add.name, new Worker(add, statusProvider));
+      }
+
+      for (const remove of removed) {
+        if (workers.has(remove.name)) {
+          workers.get(remove.name)!.dispose();
+          workers.delete(remove.name);
+        }
+      }
+    }),
   );
-  statusBarItem.command = 'code-ownership-vscode.showOwnershipInfo';
 
-  vscode.window.onDidChangeActiveTextEditor(updateStatus);
-  updateStatus(vscode.window.activeTextEditor);
+  vscode.workspace.workspaceFolders?.forEach((folder) => {
+    workers.set(folder.name, new Worker(folder, statusProvider));
+  });
 
-  context.subscriptions.push(disposable);
+  vscode.commands.executeCommand('code-ownership-vscode.run');
 
   log('info', 'Exension activated');
 }
@@ -123,103 +116,6 @@ type Owner = {
 type UserAction = {
   title: string;
   uri: vscode.Uri;
-};
-
-type Validator = (filepath: string) => Promise<Owner | undefined>;
-
-/**
- * Returns randomly mocked ownership data (useful for testing?)
- * @param filepath path of the file to check
- * @returns an {@link Owner Owner} object or `undefined` if no owner can be determined
- */
-const mockValidator: Validator = (filepath) => {
-  const ms = Math.random() * 1000;
-  const noOwner = Math.random() > 0.5;
-  const isError = Math.random() > 0.99;
-
-  return new Promise((res, rej) =>
-    setTimeout(() => {
-      if (isError) rej();
-
-      if (noOwner) res(undefined);
-
-      res({
-        filepath,
-        teamName: `Some Team`,
-        teamConfig: filepath,
-        actions: [],
-      });
-    }, ms),
-  );
-};
-
-const codeownershipValidator: Validator = async (filepath) => {
-  // bin/codeownership currenlty wants relative paths
-  const cwd = rootWorkspace()?.uri.fsPath;
-  const relativePath = relative(cwd || process.cwd(), filepath);
-
-  logSpace();
-  log('debug', `cwd: ${cwd}`);
-  log('debug', `workspace: ${rootWorkspace()?.uri.fsPath}`);
-  log('debug', `file path: ${filepath}`);
-  log('debug', `relative path: ${relativePath}`);
-
-  try {
-    const output = runCommand(
-      cwd,
-      `bin/codeownership for_file "${relativePath}" --json`,
-    );
-
-    const obj = JSON.parse(output);
-
-    if (typeof obj.team_name !== 'string') {
-      log('warning', 'Missing expected property `team_name` in command output');
-    }
-    if (typeof obj.team_yml !== 'string') {
-      log('warning', 'Missing expected property `team_yml` in command output');
-    }
-
-    if (
-      typeof obj.team_name === 'string' &&
-      typeof obj.team_yml === 'string' &&
-      obj.team_name !== 'Unowned'
-    ) {
-      const teamConfig = resolve(
-        rootWorkspace()?.uri.fsPath || '',
-        obj.team_yml,
-      );
-
-      const actions: UserAction[] = [];
-
-      const slackChannel = await getSlackChannel(teamConfig);
-
-      if (slackChannel) {
-        actions.push({
-          title: `Slack: #${slackChannel}`,
-          uri: vscode.Uri.parse(
-            `https://slack.com/app_redirect?channel=${slackChannel}`,
-          ),
-        });
-      }
-
-      actions.push({
-        title: 'View team config',
-        uri: vscode.Uri.parse(teamConfig),
-      });
-
-      return {
-        filepath,
-        teamName: obj.team_name,
-        teamConfig,
-        actions,
-      };
-    }
-    ranWithError = false;
-  } catch {
-    ranWithError = true;
-    log('error', 'Error parsing command output');
-  }
-  return undefined;
 };
 
 async function getSlackChannel(
@@ -240,28 +136,27 @@ async function getSlackChannel(
   }
 }
 
-function runCommand(
+async function runCommand(
   cwd: string | undefined,
   command: string,
-  stdin?: string,
-): string {
+  statusProvider: StatusProvider,
+): Promise<string> {
   let stdout: string = '';
   log('info', `command: ${command}`);
 
   try {
-    stdout = cp
-      .execSync(command, {
-        cwd,
-        input: stdin,
-      })
-      .toString()
-      .trim();
+    stdout = await new Promise((res, rej) =>
+      cp.exec(command, { cwd }, (err, out, stderr) => {
+        if (err) rej(err);
+        else if (typeof stderr === 'string' && stderr.length) rej(stderr);
+        else res(out.trim());
+      }),
+    );
 
     log('info', `stdout: ${stdout}`);
-    ranWithError = false;
   } catch (ex) {
-    ranWithError = true;
-    log('error', ex.message);
+    statusProvider.status = 'error';
+    log('error', ex.message || ex.toString());
   }
 
   return stdout;
@@ -271,8 +166,8 @@ function logSpace() {
   channel.appendLine('');
 }
 
-function log(level: 'debug' | 'info' | 'warning' | 'error', msg: string) {
-  channel.appendLine(`[${date()}] [${level}] ${msg}`);
+function log(level: 'debug' | 'info' | 'warning' | 'error', ...msg: string[]) {
+  channel.appendLine(`[${date()}] [${level}] ${msg.join(', ')}`);
 }
 
 function date() {
@@ -281,4 +176,156 @@ function date() {
     '.' +
     `000${new Date().getMilliseconds()}`.slice(-3)
   );
+}
+
+type Status = 'idle' | 'working' | 'error';
+
+class StatusProvider implements vscode.Disposable {
+  constructor(private readonly statusBarItem: vscode.StatusBarItem) {
+    this._listener = vscode.window.onDidChangeActiveTextEditor((active) => {
+      if (!active) this.statusBarItem.hide();
+    });
+  }
+
+  private _listener: vscode.Disposable;
+
+  private _status: Status = 'idle';
+  private _owner: Owner | undefined = undefined;
+
+  get status(): Status {
+    return this._status;
+  }
+  set status(value: Status) {
+    this._status = value;
+    this.update();
+  }
+
+  get owner(): Owner | undefined {
+    return this._owner;
+  }
+  set owner(value: Owner | undefined) {
+    this._owner = value;
+    this.update();
+  }
+
+  private update() {
+    if (this.status === 'error') {
+      this.statusBarItem.text = '$(error) Owner: Error!';
+      this.statusBarItem.tooltip = `See "${channel.name}" output channel for details`;
+      this.statusBarItem.show();
+    } else if (this.status === 'working') {
+      this.statusBarItem.text = '$(loading~spin) Owner: running...';
+      this.statusBarItem.tooltip = undefined;
+      this.statusBarItem.show();
+    } else if (this.status === 'idle') {
+      if (this.owner) {
+        this.statusBarItem.text = `$(account) Owner: ${this.owner.teamName}`;
+        this.statusBarItem.tooltip = undefined;
+        this.statusBarItem.show();
+      } else {
+        this.statusBarItem.text = `$(warning) Owner: none`;
+        this.statusBarItem.tooltip = undefined;
+        this.statusBarItem.show();
+      }
+    }
+  }
+
+  dispose() {
+    this._listener.dispose();
+  }
+}
+
+class Worker implements vscode.Disposable {
+  constructor(
+    private readonly workspace: vscode.WorkspaceFolder,
+    private readonly statusProvider: StatusProvider,
+  ) {}
+
+  workspaceHas(file: vscode.Uri): boolean {
+    return file.fsPath.startsWith(this.workspace.uri.fsPath);
+  }
+
+  async run(file: vscode.Uri): Promise<void> {
+    if (this.workspaceHas(file)) {
+      this.statusProvider.status = 'working';
+
+      await new Promise((r) => setTimeout(r, 50));
+
+      // bin/codeownership currenlty wants relative paths
+      const cwd = this.workspace.uri.fsPath;
+      const relativePath = relative(cwd, file.fsPath);
+
+      logSpace();
+      log('debug', `cwd: ${cwd}`);
+      log('debug', `workspace: ${this.workspace.uri.fsPath}`);
+      log('debug', `file path: ${file.fsPath}`);
+      log('debug', `relative path: ${relativePath}`);
+
+      try {
+        const output = await runCommand(
+          cwd,
+          `bin/codeownership for_file "${relativePath}" --json`,
+          this.statusProvider,
+        );
+
+        const obj = JSON.parse(output);
+
+        if (typeof obj.team_name !== 'string') {
+          log(
+            'warning',
+            'Missing expected property `team_name` in command output',
+          );
+        }
+        if (typeof obj.team_yml !== 'string') {
+          log(
+            'warning',
+            'Missing expected property `team_yml` in command output',
+          );
+        }
+
+        if (
+          typeof obj.team_name === 'string' &&
+          typeof obj.team_yml === 'string' &&
+          obj.team_name !== 'Unowned'
+        ) {
+          const teamConfig = resolve(this.workspace.uri.fsPath, obj.team_yml);
+
+          const actions: UserAction[] = [];
+
+          const slackChannel = await getSlackChannel(teamConfig);
+
+          if (slackChannel) {
+            actions.push({
+              title: `Slack: #${slackChannel}`,
+              uri: vscode.Uri.parse(
+                `https://slack.com/app_redirect?channel=${slackChannel}`,
+              ),
+            });
+          }
+
+          actions.push({
+            title: 'View team config',
+            uri: vscode.Uri.parse(teamConfig),
+          });
+
+          this.statusProvider.owner = {
+            filepath: file.fsPath,
+            teamName: obj.team_name,
+            teamConfig,
+            actions,
+          };
+        } else {
+          this.statusProvider.owner = undefined;
+        }
+        this.statusProvider.status = 'idle';
+      } catch {
+        this.statusProvider.status = 'error';
+        log('error', 'Error parsing command output');
+      }
+    }
+  }
+
+  dispose() {
+    // TODO
+  }
 }
