ff3b2a63ca6e2bc3671cf10bda77bb495df6a2dd
Steve Konves <stephen.konves@gmail.com>
2022-05-18 15:45:58 -0700

    chore: remove dead code

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export function reverseString(forward: string): string {
-  if (!forward) return forward;
-
-  return forward.split('').reverse().join('');
-}
-
-console.log('This is the example typescript application!');
